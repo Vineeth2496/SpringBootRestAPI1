73f3252c5cbe6754c68bb79e5af1d85145f75015
Vineeth2496 <vineetharmoori2496@gmail.com>
2024-03-30 11:09:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SpringBootRestAPI1
+ 
---
+++ src/test/java/com/springbootrestapi/SpringBootRestApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
